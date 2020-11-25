d4efaeb60916869c416f1dddfd3f1cc825c7a8f0
Alejandro Castellano <54985923+alexcl01@users.noreply.github.com>
2020-11-25 18:09:09 +0100

    Add files via upload

--- ReservationTest.java
+++ ReservationTest.java
@@ -8,12 +8,14 @@ class ReservationTest {
 	
 	Reservation Reserve = new Reservation("Alex");
 	Reservation ReserveNumber = new Reservation("Maria", 4);
+	Reservation ReserveP = new Reservation("Pepe", 2, 100);
 
 	@Test
 	void test() {
 		assertEquals("Alex", Reserve.getName());
 		assertEquals("Maria", ReserveNumber.getName());
 		assertEquals(4, ReserveNumber.getRoom());
+		assertEquals(100, ReserveP.getPrize());
 	}
 
 }
